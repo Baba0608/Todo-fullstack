020a1d54f8acb87c7327e212ec7232c532e0e91b
Baba0608 <babafakruddinnitturu@gmail.com>
2024-08-26 23:52:20 +0530

    add , update , delete Todo

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import Todo from "./Todo";
+
+let todos = [
+  { id: 1, todo: "Learn React Js", completed: true },
+  { id: 2, todo: "Learn Next Js", completed: false },
+  { id: 3, todo: "Learn Java Script", completed: true },
+  { id: 4, todo: "Learn Angular Js", completed: false },
+];
+
 const Home = () => {
+  const [todoList, setTodoList] = useState(todos);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    setTodoList([
+      ...todoList,
+      { id: todoList.length + 1, todo: todo, completed: false },
+    ]);
+
+    setTodo("");
+  };
+
+  const updateTodo = (id, todoMsg) => {
+    setTodoList((todoList) =>
+      todoList.map((todo) => {
+        if (todo.id === id) return { ...todo, todo: todoMsg };
+        else return todo;
+      })
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodoList((todoList) => todoList.filter((todo) => todo.id !== id));
+  };
+
+  console.log(todoList);
+
   return (
-    <div>
-      <div>Home</div>
+    <div className="mt-10 flex justify-center">
+      <div className="w-6/12 mt-5 shadow-lg shadow-gray-400 p-5 rounded-lg">
+        <div className="flex justify-between mb-5">
+          <input
+            className="w-10/12 bg-white rounded-md p-1"
+            value={todo}
+            placeholder="enter todo..."
+            onChange={(e) => setTodo(e.target.value)}
+          />
+
+          <button className="w-1/12 bg-blue-600 rounded-md" onClick={addTodo}>
+            Add
+          </button>
+        </div>
+
+        <hr className="border-b-2 border-gray-400 mb-5" />
+
+        <div>
+          {todoList.map((data) => (
+            <Todo
+              key={data.id}
+              data={data}
+              funcs={{ updateTodo, deleteTodo }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/Todo.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const Todo = ({
+  data: { id, todo, completed },
+  funcs: { updateTodo, deleteTodo },
+}) => {
+  const [isCompleted, setIsCompleted] = useState(completed);
+  const [isEditing, setIsEditing] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo);
+
+  const checkTodo = () => {
+    setIsCompleted(!isCompleted);
+  };
+
+  const changeText = (e) => {
+    setTodoMsg(e.target.value);
+  };
+
+  const handleTodoChange = (e) => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+      updateTodo(id, todoMsg);
+    }
+  };
+
+  const handleDeleteTodo = () => {
+    deleteTodo(id);
+  };
+
+  return (
+    <div className="flex justify-between my-2">
+      <div
+        className={`w-10/12 rounded-md py-1 px-2 flex ${
+          isCompleted ? "bg-green-300" : "bg-red-300"
+        }`}
+      >
+        <input type="checkbox" checked={isCompleted} onChange={checkTodo} />
+        <input
+          type="text"
+          autoFocus="on"
+          className={`mx-2 px-2 focus:outline-none w-full border border-b rounded-md ${
+            isCompleted ? "bg-green-300" : "bg-red-300"
+          } ${isEditing ? "border-gray-400" : "border-transparent"}`}
+          value={todoMsg}
+          onKeyUp={handleTodoChange}
+          onChange={changeText}
+          readOnly={!isEditing}
+        />
+      </div>
+      <div className="w-2/12 flex justify-around">
+        <button
+          className="w-4/12"
+          disabled={isCompleted}
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+        >
+          {isEditing ? "✅" : "✏️"}
+        </button>
+        <button className="w-4/12" onClick={handleDeleteTodo}>
+          ❌
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
